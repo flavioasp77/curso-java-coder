20080dcca20affaca28af7c5a8dffe6d7e5d141f
Flávio Pires <fpires@catitech.com.br>
2022-09-02 08:40:30 -0300

    Getters e Setters

---
+++ src/oo/encapsulamento/PessoaTeste.java
@@ -0,0 +1,15 @@
+package oo.encapsulamento;
+
+public class PessoaTeste {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(30);
+		
+		p1.setIdade(20);
+		
+		System.out.println(p1.getIdade());
+
+	}
+
+}
